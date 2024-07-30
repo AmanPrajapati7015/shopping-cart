17edbe72db2891558de6e66db5bd69a286d40315
Aman <aman.prajapati7022@gmail.com>
2024-07-30 20:45:17 +0530

    responsive navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -2,11 +2,13 @@ import logo from '/navbar/logo.png'
 import profileLogo from '/navbar/profile.svg'
 import favLogo from '/navbar/fav.svg'
 import bagLogo from '/navbar/bag.svg'
+import menuIcon from '/navbar/menu.svg'
 
 import {toTitleCase} from './utils'
 import styles from './styles/navbar.module.css'
 
 import categoriesMap from './data/categories'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const sectionsArray = [];
@@ -20,10 +22,18 @@ for(let key in categoriesMap){
 export default function Navbar(){
     
     const navigator = useNavigate();
+    
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     function handleClick(e){
+        // console.log(e.target);
         let section = e.target.textContent.toLowerCase();
         navigator('/category/'+ section);
+        setIsMenuOpen(false);
     }
 
     return (
@@ -34,11 +44,9 @@ export default function Navbar(){
             </div>
 
             <div className={styles.middleSection}>
-            {
-                sectionsArray.map((section, i)=>{
+                {sectionsArray.map((section, i)=>{
                     return <h2 key={i} className={styles.sectionItem} onClick={handleClick}>{toTitleCase(section)}</h2>
-                })
-            }
+                })}
             </div>
 
             <div className={styles.icons}>
@@ -47,7 +55,22 @@ export default function Navbar(){
                 <img src={bagLogo} alt="" onClick={()=>navigator('/bag')} />
             </div>
 
-            
+            <div className={styles.menuButton} onClick={handleMenuToggle}>
+                <img src={menuIcon} alt="" />
+            </div>
+
+            {/* Responsive Menu */}
+            {isMenuOpen && (
+                <div className={styles.mobileMenu}>
+                    {sectionsArray.map((section, i) => (
+                        <h2 key={i} className={styles.sectionItem} onClick={handleClick} >{toTitleCase(section)}</h2>
+                    ))}
+                    <hr className={styles.hr}/>
+                    <h2>Profile</h2>
+                    <h2>Favorite</h2>
+                    <h2 onClick={() => {navigator('/bag');setIsMenuOpen(false)}} >Bag</h2>
+                </div>
+            )}
         </nav>
     )
 }
